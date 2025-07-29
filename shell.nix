a9019f24a97812c0b1d94f261357237cb9a809ee
tomoron <tomoron@student.42angouleme.fr>
2025-07-29 14:31:51 +0200

    use older nixpkgs version in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ ... }:
 
+let
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/029dea9aaacf.tar.gz";
+  }) {};
+in
 pkgs.mkShell {
   nativeBuildInputs =
     with pkgs; [
         pkgsCross.i686-embedded.buildPackages.gcc
         pkgsCross.i686-embedded.buildPackages.grub2
 		mtools
+		libisoburn
     ];
 }
